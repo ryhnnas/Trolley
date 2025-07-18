89d85325c4199e0d5f551fe498eeee7e1ac45891
Reyhan Agra Syihab <agrahans13@gmail.com>
2025-07-18 21:10:24 +0700

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,7 @@
 import io from 'socket.io-client';
 
-export const socket = io('http://localhost:5000');
+const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+
+export const socket = io(URL, {
+    autoConnect: false
+});
